aa84b6f2aa604b405b9d2cad4b76d2552234aaf2
janhajk <janhajk@gmail.com>
2020-02-14 21:26:22 +0000

    bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@
 radiumpath=$HOME/radium
 
 mkdir -p $radiumpath
-wget --no-check-certificate --directory-prefix=$radiumpath/ $CLIENT_URL
+wget --no-check-certificate --directory-prefix=$HOME/ $CLIENT_URL
 
-
-tar xzvf $CLIENT_NAME.tar.gz -C $radiumpath
+tar xzvf $HOME/$CLIENT_NAME.tar.gz -C $radiumpath
 pushd $radiumpath/radium-0.11-$CLIENT_NAME/src
 
 qmake && make
+
