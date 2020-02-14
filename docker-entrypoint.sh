7d151948a6b8c0a3668b0a00c13f469bf4c97cf1
janhajk <janhajk@gmail.com>
2020-02-14 16:46:12 +0000

    entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p $HOME/radium
 wget $CLIENT_URL
 
 
 tar xzvf $CLIENT_NAME.tar.gz -C ${HOME}/radium
-cd $HOME/radium/radium-0.11-1.5.1.0/src
+pushd $HOME/radium/radium-0.11-1.5.1.0/src
 
 qmake && make
