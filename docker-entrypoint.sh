0068ec7e1f30741e95520669745d05265e013ca1
janhajk <janhajk@gmail.com>
2020-02-15 14:00:32 +0000

    bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-/home/radium/radium/radium-0.11-1.5.1.0/src/radiumd > /dev/null &
+echo "Bis hier bin ich noch gekommen"
+#/home/radium/radium/radium-0.11-1.5.1.0/src/radiumd > /dev/null &
+
 
