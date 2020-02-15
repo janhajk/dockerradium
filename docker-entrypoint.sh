3b3435a1e10319c90c32da0d60b8d9d52eba6dbc
janhajk <janhajk@gmail.com>
2020-02-15 14:41:37 +0000

    bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash -e
 
-echo "Bis hier bin ich noch gekommen"
-#/home/radium/radium/radium-0.11-1.5.1.0/src/radiumd > /dev/null &
+echo "Starting Radium Daemon..."
+/home/radium/radium/radium-0.11-1.5.1.0/src/radiumd > /dev/null &
 
 
+echo "Waiting for daemon..."
+sleep 60
